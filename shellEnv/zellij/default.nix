37afb0474c069bf83c2401825fd874820dd8f62e
kosumi <chankocyo@gmail.com>
2023-06-03 15:56:38 -0600

    Fix zellij keybinds

--- shellEnv/zellij/default.nix
+++ shellEnv/zellij/default.nix
@@ -1,31 +1,32 @@
 { pkgs, ... }: {
   programs.zellij = {
     enable = true;
-    enableFishIntegration = true;
+    #enableFishIntegration = true;
     settings = {
       default_shell = "${pkgs.fish}/bin/fish";
       theme = "Tokyo Night Storm";
       copy_command = "xclip -selection clipboard";
-      keybinds = {
-        normal = {
-          #"bind \"Ctrl 4\"" = {NewTab = ";"; };
-        };
-      };
+      #keybinds = {
+        #normal = {
+          ##"bind \"Ctrl 4\"" = {NewTab = ";"; };
+        #};
+      #};
     };
   };
   xdg.configFile."zellij/config.kdl".text = ''
     keybinds {
+      unbind "Ctrl t"
       normal {
-          bind "Ctrl 4" {NewTab;}
-        }
+        bind "Ctrl u" {SwitchToMode "tab"; }
+      }
       pane {
-          bind "Ctrl 4" {NewTab;}
+          bind "Ctrl u" {SwitchToMode "tab"; }
         }
       tab {
-          bind "Ctrl 4" {NewTab;}
+          bind "Ctrl u" {SwitchToMode "tab"; }
         }
       session {
-          bind "Ctrl 4" {NewTab;}
+          bind "Ctrl u" {SwitchToMode "tab"; }
         }
       }
   '';
