62437ffaed374d99ab5269d0e7bee812549da91b
kosumi <chankocyo@gmail.com>
2023-06-04 23:47:05 -0600

    Disable copy command

--- shellEnv/zellij/default.nix
+++ shellEnv/zellij/default.nix
@@ -1,11 +1,11 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   programs.zellij = {
     enable = true;
     #enableFishIntegration = true;
     settings = {
       default_shell = "${pkgs.fish}/bin/fish";
       theme = "Tokyo Night Storm";
-      copy_command = "xclip -selection clipboard";
+      # copy_command = "xclip -selection clipboard";
       #keybinds = {
       #normal = {
       ##"bind \"Ctrl 4\"" = {NewTab = ";"; };
