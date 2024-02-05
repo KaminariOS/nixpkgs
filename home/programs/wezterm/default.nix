331b694a1174a1b64fba2bef063843f7beefea69
kosumi <chankocyo@gmail.com>
2024-02-05 12:48:53 -0700

    Wezterm implicit link

--- home/programs/wezterm/default.nix
+++ home/programs/wezterm/default.nix
@@ -119,6 +119,19 @@
             bottom = '0.1cell',
           }
 
+
+          -- Use the defaults as a base
+          config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
+          -- make username/project paths clickable. this implies paths like the following are for github.
+          -- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
+          -- as long as a full url hyperlink regex exists above this it should not match a full url to
+          -- github or gitlab / bitbucket (i.e. https://gitlab.com/user/project.git is still a whole clickable url)
+          table.insert(config.hyperlink_rules, {
+            regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+            format = 'https://www.github.com/$1/$3',
+          })
+
            -- and finally, return the configuration to wezterm
            return config
     '';
