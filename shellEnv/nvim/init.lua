2dab6be4692e835011bd62bf33cf7800c7f3b52c
kosumi <chankocyo@gmail.com>
2024-03-18 14:57:28 -0600

    UFO depre

--- shellEnv/nvim/init.lua
+++ shellEnv/nvim/init.lua
@@ -179,7 +179,7 @@ end
 
 require('ufo').setup({
     open_fold_hl_timeout = 150,
-    close_fold_kinds = {'imports', 'comment'},
+    close_fold_kinds_for_ft = {'imports', 'comment'},
     preview = {
         win_config = {
             border = {'', '─', '', '', '', '─', '', ''},
