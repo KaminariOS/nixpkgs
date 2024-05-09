3ddf15209da72415312d07c549864aa1326608d0
kosumi <chankocyo@gmail.com>
2024-05-09 16:41:29 -0600

    Add shellhome for work

--- outputs/home-conf.nix
+++ outputs/home-conf.nix
@@ -69,6 +69,7 @@ in
   kosumi = let username = "kosumi"; in mkHome { hidpi = false;inherit username; homed = "/home/${username}"; };
 
   shellhome = let username = "Kosumi"; in mkHome { hidpi = false;inherit username; shell = true; homed = "/users/${username}"; };
+  shellhomeForWork = let username = "kchen"; in mkHome { hidpi = false;inherit username; shell = true; homed = "/home/${username}"; };
   # Continuos Integration automation
   #  ci = {
   #    metals = pkgs.callPackage ../home/programs/neovim-ide/metals.nix { };
